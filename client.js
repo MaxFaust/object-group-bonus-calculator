91da07a3f66cb68c477608eaffa07965482046ac
Max Faust <tcthinker@gmail.com>
2020-03-04 16:00:06 -0600

    math sussed out but nothing works

--- client.js
+++ client.js
@@ -29,7 +29,7 @@ const employees = [
     annualSalary: '35000',
     reviewRating: 1
   }
-];
+];//end employee array
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
@@ -40,4 +40,50 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+console.log(employees);
+
+//bonus Array
+const personBonus = [];
+
+function bonusCalculator(person) {
+  console.log(person);
+
+  //establish calculated bonus %
+  let totalBonus = 0;
+
+  //bonus % calculator
+  if (person.reviewRating <= 2) {
+    totalBonus = (0);
+  } else if (person.reviewRating === 3) {
+    totalBonus = .04;
+  } else if (person.reviewRating === 4) {
+    totalBonus = .06;
+  } else if (person.reviewRating === 5) {
+    totalBonus = .1;
+  }
+  //employee number bonus 
+  if (person.employeeNumber.length === 4) {
+    totalBonus = totalBonus + .05;
+  }
+  //above 65,000 adjust down 1%
+  if (person.annualSalary > 65000) {
+    totalBonus -= .01;
+  }
+  if (totalBonus < 0) {
+    totalBonus = 0;
+  }
+  if (totalBonus > .13) {
+    totalBonus = .13;
+  }
+}
+//make the object
+let newPerson = {
+  name: employees.name,
+  bonusPercentage: totalBonus,
+  totalCompensation: (person.annualSalary * totalBonus) + person.annualSalary,
+  totalBonus: person.annualSalary * totalBonus
+}
+
+console.log(totalBonus);
+console.log('employee bonus is:', bonusCalculator(employees[0]));
+console.log('employee bonus is:', bonusCalculator('Mayella'));
